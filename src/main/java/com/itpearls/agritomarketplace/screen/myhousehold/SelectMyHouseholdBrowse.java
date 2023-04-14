181242209c10d6f9fd9844570ad0e151e36811d5
Aleksey Ananyev <deneb@yandex.ru>
2023-04-14 09:58:23 +0400

    SelectMyHouseholdBrowse - колонка роль

--- src/main/java/com/itpearls/agritomarketplace/screen/myhousehold/SelectMyHouseholdBrowse.java
+++ src/main/java/com/itpearls/agritomarketplace/screen/myhousehold/SelectMyHouseholdBrowse.java
@@ -1,6 +1,12 @@
 package com.itpearls.agritomarketplace.screen.myhousehold;
 
+import com.itpearls.agritomarketplace.entity.AgriculturalManufacturer;
+import com.itpearls.agritomarketplace.entity.TradeRole;
+import io.jmix.core.DataManager;
 import io.jmix.core.security.CurrentAuthentication;
+import io.jmix.ui.UiComponents;
+import io.jmix.ui.component.Component;
+import io.jmix.ui.component.Label;
 import io.jmix.ui.model.CollectionLoader;
 import io.jmix.ui.screen.*;
 import com.itpearls.agritomarketplace.entity.MyHousehold;
@@ -10,14 +16,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 @UiDescriptor("select-my-household-browse.xml")
 @LookupComponent("myHouseholdsTable")
 public class SelectMyHouseholdBrowse extends StandardLookup<MyHousehold> {
+
     @Autowired
     private CollectionLoader<MyHousehold> myHouseholdsDl;
     @Autowired
     private CurrentAuthentication currentAuthentication;
+    @Autowired
+    private UiComponents uiComponents;
+    @Autowired
+    private DataManager dataManager;
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
         myHouseholdsDl.setParameter("owner", currentAuthentication.getUser());
         myHouseholdsDl.load();
+    }
+
+    @Install(to = "myHouseholdsTable.counterPartyType", subject = "columnGenerator")
+    private Component myHouseholdsTableCounterPartyTypeColumnGenerator(MyHousehold myHousehold) {
+        Label retLabel = uiComponents.create(Label.class);
+
+        if (dataManager.load(AgriculturalManufacturer.class)
+                .query("select e from AgriculturalManufacturer e where e.counterpartyName = :companyName")
+                .parameter("companyName", myHousehold.getCounterpartyName())
+                .list().size() > 0) {
+            retLabel.setValue(TradeRole.SELLER);
+        } else {
+            retLabel.setValue(TradeRole.BUYER);
+        }
+
+        return retLabel;
     }
 }
